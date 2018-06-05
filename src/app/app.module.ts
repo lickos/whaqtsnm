1b750b09027e8b8def651221c11209d122ceb289
lickos <george.chios@gmail.com>
2018-06-05 15:09:58 +0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
+import { AngularFireModule } from "angularfire2";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { AngularFireAuthModule } from "angularfire2/auth";
+import { HttpClientModule } from "@angular/common/http";
+import { Geolocation } from "@ionic-native/geolocation";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { MyApp } from "./app.component";
+import { HomePage } from "../pages/home/home";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCba12QLFvjUpH5yDVtOUICrtJzhnQuX3U",
+  authDomain: "whatsnearme-204220.firebaseapp.com",
+  databaseURL: "https://whatsnearme-204220.firebaseio.com",
+  projectId: "whatsnearme-204220",
+  storageBucket: "whatsnearme-204220.appspot.com",
+  messagingSenderId: "418998884987"
+};
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
+  declarations: [MyApp, HomePage],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+  entryComponents: [MyApp, HomePage],
+  providers: [StatusBar, SplashScreen, Geolocation, { provide: ErrorHandler, useClass: IonicErrorHandler }]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,71 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { AngularFireAuth } from "angularfire2/auth";
+import firebase from "firebase/app";
+import { HttpClient } from "@angular/common/http";
+import { Geolocation } from "@ionic-native/geolocation";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  displayName;
+  credentials: any;
+  accessToken: string;
+  loc: string;
+  constructor(
+    public navCtrl: NavController,
+    public geolocation: Geolocation,
+    public afAuth: AngularFireAuth,
+    private http: HttpClient
+  ) {
+    this.geolocation
+      .getCurrentPosition()
+      .then(resp => {
+        let lat = resp.coords.latitude.toString();
+        let lng = resp.coords.longitude.toString();
+        this.loc = lat + "," + lng;
+        afAuth.authState.subscribe(user => {
+          if (!user) {
+            this.displayName = null;
+            return;
+          } else {
+            this.displayName = user.displayName;
+            this.accessToken = user.refreshToken;
+            console.log(this.accessToken);
+            setTimeout(this.getPlaces(this.accessToken), 2000);
+            console.log(user);
+          }
+        });
+      })
+      .catch(error => {
+        console.log("Error getting location", error);
+      });
   }
 
+  signInWithFacebook() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider()).then(res => {
+      this.credentials = res;
+      this.accessToken = this.credentials.credential.accessToken;
+      this.getPlaces(this.accessToken);
+    });
+  }
+
+  signOut() {
+    this.afAuth.auth.signOut();
+  }
+
+  getPlaces(accessToken) {
+    let url =
+      "https://graph.facebook.com/search?type=place&access_token=" +
+      accessToken +
+      "&fields=name,checkins,picture&center=" +
+      this.loc +
+      "&distance=1000";
+    console.log(url);
+    this.http.get(url).subscribe(res => {
+      console.log(res);
+    });
+  }
 }
